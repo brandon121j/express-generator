5943bb7cb275ed431028e32fbefbbf7a2c2792dd
Brandon <brandon121j@DESKTOP-DPFP3JR.localdomain>
2021-09-20 15:11:54 -0400

    Addition of get request

--- routes/animal.js
+++ routes/animal.js
@@ -17,7 +17,45 @@ let animals = [
 ]
 
 router.get('/', function(req, res, next) {
+    let foundAnimal = null;
+
+    if (Object.keys(req.query).length === 0) {
+        res.json({ animals })
+    } else {
+        animals.forEach(item => {
+            if (item.animalType === req.query.animalType.toLowerCase()) {
+                foundAnimal = item;
+            }
+        })
+
+        if (!foundAnimal) {
+            return res.status(404).json({ message: "No animal found" })
+        } else {
+            res.json({message: "success", foundAnimal});
+        }
+    }
     res.json({ animals })
+});
+
+router.get('/get-animal-by-id/:id', function(req, res) {
+    
+    const { id } = req.params;
+    let foundAnimal = null;
+
+    animals.forEach(function(item) {
+
+        if (item.id === +id) {
+            foundAnimal = item;
+        }
+    })
+    
+    if (!foundAnimal) {
+        res
+            .status(404)
+            .json({ message: "Item not found" })
+    } else {
+        res.json({ foundAnimal})
+    }
 });
 
 module.exports = router;
